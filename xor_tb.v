b149a625755d770fe18515a04968e6ba25c63c4f
CRIME <161829820+MohsenCRIME@users.noreply.github.com>
2024-07-18 07:25:43 -0700

    this is a verilog file
    I want to try to learn how to work with githup

---
+++ xor_tb.v
@@ -0,0 +1,39 @@
+`timescale 1ns/1ns
+`include "xor.v"
+
+module xor_tb();
+
+    reg a , b;
+    wire out;
+
+    xor_gate g1(a , b , out);
+
+    initial begin
+
+        $dumpfile("xor_tb.vcd");
+        $dumpvars(0, xor_tb);
+        
+        a = 1;
+        b = 1;
+
+        #10;
+
+        a = 0;
+        b = 1;
+
+        #10;
+
+        a = 1;
+        b = 0;
+
+        #10;
+
+        a = 1;
+        b = 1;
+
+        #10;
+
+    end
+
+
+endmodule
